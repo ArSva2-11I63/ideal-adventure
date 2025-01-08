99dbc2f03ea518cb9a309e64db5918253b2b7d21
ArSva2-11I63 <aarnaytc1@gmail.com>
2025-01-08 18:39:24 +0530

    Add files via upload

---
+++ ay.py
@@ -0,0 +1,11 @@
+def printnumber(n):
+    iteration = 0
+    print('The number entered by the user is {}'.format(n))
+    iteration = iteration + 1
+    print("Total iterations done by the code: ",iteration,"\n")
+
+printnumber(1)
+printnumber(2)
+printnumber(3)
+
+print("As we can see, there is no change in time taken by the code for any n")
