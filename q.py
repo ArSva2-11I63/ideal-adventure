30c4c75f34de75b4504c107a097f72ae29e3d9ba
ArSva2-11I63 <aarnaytc1@gmail.com>
2025-01-10 17:20:18 +0530

    Add files via upload

--- q.py
+++ q.py
@@ -1,15 +1,8 @@
-def printnumber(n):
-    print("The value of n is",n)
-    iteration = 0
-    for i in range(0, n+1):
-        for j in range (0, n+1):
-            iteration +=1
+def prints(n):
+    if (n<=0):
+        return
+    print("Hello")
+    prints(n/2)
+    prints(n/2)
 
-    print("Total number of iterations is {}x{} = {}".format(n, n, n*n))
-    print("\n")
-
-printnumber(1)
-printnumber(5)
-printnumber(10)
-
-print("\nThe time taken increases with increase in n^2")
+prints(2)
